8af5f3631fe1fff881f5c144d5f9675d3b485525
mahesh0790 <mahesh.rockz790@gmail.com>
2017-11-10 12:08:35 +0530

    commiting the file

---
+++ ifcondition.sh
@@ -0,0 +1,15 @@
+
+echo -n "enter the the name of the team:"
+
+read name
+ 
+
+if [ $name = QA ]
+then
+echo "i will connect secure connection to the remote server-1"
+elif [ $name = SIT ]
+then
+echo "i will connect secure connection to the remote server-1"
+else 
+echo "end"
+fi
